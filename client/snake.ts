2ffede0133169bf4df10eb8f480b5549f9020eaf
Paul Ronga <gen@paulronga.ch>
2024-10-17 20:51:03 +0200

    snake fine-tuning

--- client/snake.ts
+++ client/snake.ts
@@ -5,8 +5,10 @@ interface Point {
     y: number
 }
 
-interface SnakeInterface extends Phaser.GameObjects.Group {
+export interface SnakeInterface extends Phaser.GameObjects.Group {
     moveTo(x: number, y: number): void
+    bodies: any[]
+    physicsGroup: Phaser.Physics.Arcade.Group
 }
 
 export class Snake extends Phaser.GameObjects.Group implements SnakeInterface {
@@ -14,11 +16,12 @@ export class Snake extends Phaser.GameObjects.Group implements SnakeInterface {
     y: number
     length = 20
     bodies: any[]
+    public physicsGroup: Phaser.Physics.Arcade.Group
 
     constructor(scene: Phaser.Scene, x: number, y: number, user = false, direction = 'right') {
         super(scene)
         this.bodies = []
-        
+
         const radius = 10;
         const spacing = 2;
 
@@ -30,10 +33,19 @@ export class Snake extends Phaser.GameObjects.Group implements SnakeInterface {
             this.bodies.push(body)
             x += spacing;
         }
+        // this.physicsGroup = scene.physics.add.group(this.bodies)
     }
 
-    moveTo(x: number, y: number) {
-        Phaser.Actions.ShiftPosition(this.bodies, x, y);
+    public moveTo(x: number, y: number) {
+        // function logX(bodies){
+        //     let output = ''
+        //     for(let body of bodies){
+        //         output += `${body.x},`
+        //     }
+        //     console.log(output)
+        // }
+        // console.log('Before:', logX(this.bodies))
+        Phaser.Actions.ShiftPosition(this.bodies, x, y, 1);
     }
 
     growBy(length: number) {
